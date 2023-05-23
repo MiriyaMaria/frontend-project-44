43348ce584a9cf806117b39e38647e25c13bd91f
Maria Churilova <m.churilova.v@gmail.com>
2023-05-23 13:46:19 +0300

    change calculation in brain-calc

--- games/calc.js
+++ games/calc.js
@@ -1,19 +1,41 @@
 import readlineSync from 'readline-sync';
-import { playerName, getName } from '../src/index.js';
 import { getRandom, randomElement } from '../src/utils.js';
 
 const rule = 'What is the result of the expression?';
+
+let playerName;
+const getName = () => {
+  console.log('Welcome to the Brain Games!');
+  playerName = readlineSync.question('May I have your name? ');
+};
 
 let playerAnswer;
 let correctAnswer;
 
 const operators = ['+', '-', '*'];
 
+const doMath = (str) => {
+  let result = 0;
+  if (str.includes('+')) {
+    const operand = str.split('+');
+    result = +operand[0] + +operand[1];
+  }
+  if (str.includes('-')) {
+    const operand = str.split('-');
+    result = +operand[0] - +operand[1];
+  }
+  if (str.includes('*')) {
+    const operand = str.split('*');
+    result = +operand[0] * +operand[1];
+  }
+  return result;
+};
+
 export const getAnswer = () => {
   const randomExpression = getRandom(1, 20) + randomElement(operators) + getRandom(1, 20);
   console.log(`Question: ${randomExpression}`);
   playerAnswer = readlineSync.question('Your answer: ');
-  correctAnswer = eval(randomExpression);
+  correctAnswer = doMath(randomExpression);
 };
 
 const playCalc = () => {
